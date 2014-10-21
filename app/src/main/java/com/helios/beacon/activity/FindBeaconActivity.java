7ba7ad5858589f7f06f8b68142b95e58e46006bc
nhantran <trdainhan@gmail.com>
2014-10-21 09:25:45 +0700

    tuesday

--- app/src/main/java/com/helios/beacon/activity/FindBeaconActivity.java
+++ app/src/main/java/com/helios/beacon/activity/FindBeaconActivity.java
@@ -1,6 +1,8 @@
 package com.helios.beacon.activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.util.Log;
@@ -13,6 +15,7 @@ import com.example.nhantran.beaconexample.R;
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.MonitorNotifier;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
@@ -22,21 +25,77 @@ import java.util.Collection;
 public class FindBeaconActivity extends Activity implements BeaconConsumer {
 
     protected static final String TAG = "FindBeaconActivity";
-    private BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
+    private BeaconManager beaconManager = BeaconManager.getInstanceForApplication(getApplication());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beacon);
 
+        verifyBluetooth();
+        Log.d(TAG, "Bind");
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
         beaconManager.bind(this);
+    }
 
+    private void verifyBluetooth() {
+
+        try {
+            if (!BeaconManager.getInstanceForApplication(this).checkAvailability()) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Bluetooth not enabled");
+                builder.setMessage("Please enable bluetooth in settings and restart this application.");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        finish();
+                        System.exit(0);
+                    }
+                });
+                builder.show();
+            }
+        } catch (RuntimeException e) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Bluetooth LE not available");
+            builder.setMessage("Sorry, this device does not support Bluetooth LE.");
+            builder.setPositiveButton(android.R.string.ok, null);
+            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+                    finish();
+                    System.exit(0);
+                }
+
+            });
+            builder.show();
+
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (beaconManager.isBound(this)){
+            beaconManager.setBackgroundMode(true);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (beaconManager.isBound(this)){
+            beaconManager.setBackgroundMode(false);
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
+        Log.d(TAG, "Unbind");
         beaconManager.unbind(this);
     }
 
@@ -60,38 +119,49 @@ public class FindBeaconActivity extends Activity implements BeaconConsumer {
         return super.onOptionsItemSelected(item);
     }
 
+
     @Override
     public void onBeaconServiceConnect() {
         beaconManager.setMonitorNotifier(new MonitorNotifier() {
             @Override
             public void didEnterRegion(Region region) {
-                Log.i(TAG, "I just saw an beacon for the first time!");
+                String msg = "I just saw an beacon for the first time!";
+                Log.d(TAG, msg);
+
+                if (region != null){
+                    Log.d(TAG, region.getId1().toString());
+                }
+
             }
 
             @Override
             public void didExitRegion(Region region) {
-                Log.i(TAG, "I no longer see an beacon");
+                String msg = "I no longer see an beacon";
+                Log.d(TAG, msg);
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void didDetermineStateForRegion(int state, Region region) {
-                Log.i(TAG, "I have just switched from seeing/not seeing beacons: " + state);
+                Log.d(TAG, "I have just switched from seeing/not seeing beacons: " + state);
             }
         });
 
         beaconManager.setRangeNotifier(new RangeNotifier() {
             @Override
             public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-                if (beacons.size() > 0){
-                    Log.i(TAG, "The first beacon I see is about "+beacons.iterator().next().getDistance()+" meters away.");
+                if (beacons.size() > 0) {
+                    Log.d(TAG, "The first beacon I see is about "+beacons.iterator().next().getDistance()+" meters away.");
                 }
             }
         });
 
         try {
             beaconManager.startMonitoringBeaconsInRegion(new Region("myMonitoringUniqueId", null, null, null));
-        } catch (RemoteException e) {
+        } catch (RemoteException e) {}
 
-        }
+        try {
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {}
     }
 }
--- app/src/main/java/com/helios/beacon/application/BeaconApplication.java
+++ app/src/main/java/com/helios/beacon/application/BeaconApplication.java
@@ -1,36 +1,58 @@
 package com.helios.beacon.application;
 
 import android.app.Application;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
+import android.os.RemoteException;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.example.nhantran.beaconexample.R;
 import com.helios.beacon.activity.MainActivity;
 
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
 import org.altbeacon.beacon.startup.BootstrapNotifier;
 import org.altbeacon.beacon.startup.RegionBootstrap;
 import org.altbeacon.beacon.Region;
 
+import java.util.Collection;
+
 /**
  * Created by nhantran on 10/17/14.
  */
-public class BeaconApplication extends Application implements BootstrapNotifier {
+public class BeaconApplication extends Application implements BootstrapNotifier, RangeNotifier {
 
     private static final String TAG = "BeaconApplication";
     private RegionBootstrap regionBootstrap;
-
     private BackgroundPowerSaver backgroundPowerSaver;
+    private boolean haveDetectedBeaconsSinceBoot = false;
+
 
     @Override
     public void onCreate() {
         super.onCreate();
         Log.d(TAG, "App started up");
         // wake up the app when any beacon is seen (you can specify specific id filers in the parameters below)
-        Region region = new Region("com.example.myapp.boostrapRegion", null, null, null);
+        Region region = new Region("backgroundRegion", null, null, null);
         regionBootstrap = new RegionBootstrap(this, region);
 
+        BeaconManager beaconManager =  BeaconManager.getInstanceForApplication(this);
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+        beaconManager.setBackgroundScanPeriod(1100l);
+        beaconManager.setBackgroundBetweenScanPeriod(10000l);
+
         // enables auto battery saving of about 60%
-        backgroundPowerSaver = new BackgroundPowerSaver(this);
+//        backgroundPowerSaver = new BackgroundPowerSaver(this);
     }
 
     @Override
@@ -40,19 +62,58 @@ public class BeaconApplication extends Application implements BootstrapNotifier 
 
     @Override
     public void didEnterRegion(Region arg0) {
-        Log.d(TAG, "Got a didEnterRegion call");
-        // This call to disable will make it so the activity below only gets launched the first time a beacon is seen (until the next time the app is launched)
-        // if you want the Activity to launch every single time beacons come into view, remove this call.
-        regionBootstrap.disable();
-        Intent intent = new Intent(this, MainActivity.class);
-        // IMPORTANT: in the AndroidManifest.xml definition of this activity, you must set android:launchMode="singleInstance" or you will get two instances
-        // created when a user launches the activity manually and it gets launched from here.
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        this.startActivity(intent);
+        // In this example, this class sends a notification to the user whenever a Beacon
+        // matching a Region (defined above) are first seen.
+        Log.d(TAG, "did enter region.");
+        if (!haveDetectedBeaconsSinceBoot) {
+            Log.d(TAG, "auto launching MainActivity");
+
+            // The very first time since boot that we detect an beacon, we launch the
+            // MainActivity
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            // Important:  make sure to add android:launchMode="singleInstance" in the manifest
+            // to keep multiple copies of this activity from getting created if the user has
+            // already manually launched the app.
+            this.startActivity(intent);
+            haveDetectedBeaconsSinceBoot = true;
+        } else {
+            // If we have already seen beacons and launched the MainActivity before, we simply
+            // send a notification to the user on subsequent detections.
+            Log.d(TAG, "Sending notification.");
+            sendNotification();
+        }
+
     }
 
     @Override
     public void didExitRegion(Region arg0) {
         // Don't care
+        Log.d(TAG, "Did exit region");
+    }
+
+    private void sendNotification() {
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setContentTitle("Beacon Reference Application")
+                        .setContentText("An beacon is nearby.")
+                        .setSmallIcon(R.drawable.ic_launcher);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntent(new Intent(this, MainActivity.class));
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        builder.setContentIntent(resultPendingIntent);
+        NotificationManager notificationManager =
+                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, builder.build());
+    }
+
+    @Override
+    public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+        Log.d(TAG, "in Range region");
     }
 }
